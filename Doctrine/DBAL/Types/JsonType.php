4055afa5680a84f53b010205c1eba1f96224f218
Eugene Myazin <meniam@gmail.com>
2019-12-18 01:24:26 +0300

    Db

---
+++ Doctrine/DBAL/Types/JsonType.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Evirma\Bundle\CoreBundle\Doctrine\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use function is_resource;
+use function json_decode;
+use function json_encode;
+use function json_last_error;
+use function json_last_error_msg;
+use function stream_get_contents;
+use const JSON_ERROR_NONE;
+use const JSON_UNESCAPED_UNICODE;
+
+/**
+ * Type generating json objects values
+ */
+class JsonType extends Type
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return $platform->getJsonTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws ConversionException
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        $encoded = json_encode($value, JSON_UNESCAPED_UNICODE);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw ConversionException::conversionFailedSerialization($value, 'json', json_last_error_msg());
+        }
+
+        return $encoded;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws ConversionException
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null || $value === '') {
+            return null;
+        }
+
+        if (is_resource($value)) {
+            $value = stream_get_contents($value);
+        }
+
+        $val = json_decode($value, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw ConversionException::conversionFailed($value, $this->getName());
+        }
+
+        return $val;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'json';
+    }
+}
