444319fe868a97e6c1e0b10deb1b0999e953b6cf
Eugene Myazin <meniam@gmail.com>
2019-12-26 21:24:04 +0300

    MetaDescription Filter

--- Filter/Rule/MetaDescription.php
+++ Filter/Rule/MetaDescription.php
@@ -4,15 +4,17 @@ namespace Evirma\Bundle\CoreBundle\Filter\Rule;
 
 use Evirma\Bundle\CoreBundle\Filter\FilterRule;
 use Evirma\Bundle\CoreBundle\Filter\FilterStatic;
+use Evirma\Bundle\CoreBundle\Util\StringUtil;
 
-class MetaTrim extends FilterRule
+class MetaDescription extends FilterRule
 {
     public function filter($value)
     {
+        preg_replace('#^\s*<h\d+[^>]>(.*?)</h\d+>#usi', '', $value);
         $value = FilterStatic::filterValue($value, HtmlAndUnicode::class);
-        $value = preg_replace('#\s*([;:.,!?])#usi', '\\1', $value);
         $value = trim(preg_replace('#\s+#', ' ', $value));
+        $value = preg_replace('#\s*([;:.,!?])#usi', '\\1', $value);
 
-        return mb_strtoupper(mb_substr($value, 0, 1, 'UTF-8')).mb_substr($value, 1, mb_strlen($value, 'UTF-8'), 'UTF-8');
+        return StringUtil::truncate($value, 200, true, '');
     }
 }
