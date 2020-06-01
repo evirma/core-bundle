2c7856bbd457c78be1859cef6c2573d51c3611c5
Eugene Myazin <meniam@gmail.com>
2020-06-02 01:50:18 +0300

    NameFilter Fix

--- Filter/Rule/Name.php
+++ Filter/Rule/Name.php
@@ -12,7 +12,7 @@ class Name extends FilterRule
         $value = FilterStatic::filterValue($value, HtmlAndUnicode::class);
         $value = preg_replace('#\b(\w+\b|\w+)\s*\.\.\.\s*$#usi', '', $value);
         $value = preg_replace('#(\S)\'(s)#si', '\\1\\2', $value);
-        $value = preg_replace('#[^\w\d\)\]]+$#usi', ' ', $value);
+        $value = preg_replace('#[^\w\d)\]\"«»]+$#usi', ' ', $value);
         $value = preg_replace('#\s+#', ' ', $value);
         $value = FilterStatic::filterValue(trim($value, "\r\n\t,:;(^$ "), Ucfirst::class);
         $value = preg_replace('#\s+(это|как|так|и|в|над|к|до|не|на|но|за|то|с|ли|а|во|от|со|для|о|же|ну|вы|бы|что|кто|он|при|около|она)$#usi', '', $value);
