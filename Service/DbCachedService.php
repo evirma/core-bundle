980d376b820f7a18245d47596c9715f2416a415d
Eugene Myazin <meniam@gmail.com>
2020-06-06 13:43:52 +0300

    Db Cache

--- Service/DbCachedService.php
+++ Service/DbCachedService.php
@@ -2,6 +2,7 @@
 
 namespace Evirma\Bundle\CoreBundle\Service;
 
+use Doctrine\DBAL\Driver\Statement;
 use Evirma\Bundle\CoreBundle\Traits\CacheTrait;
 
 final class DbCachedService
@@ -44,6 +45,79 @@ final class DbCachedService
 
         if ($result = $this->db->fetchColumn($statement, $params, $column, $types, $isSlave)) {
             $this->setCacheItem($cacheId, $result, $this->ttl);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Prepares and executes an SQL query and returns the first row of the result
+     * as an associative array.
+     *
+     * @param string $statement The SQL query.
+     * @param array  $params    The query parameters.
+     * @param array  $types     The query parameter types.
+     * @param bool   $isSlave
+     * @return array|bool False is returned if no rows are found.
+     */
+    public function fetchAssoc($statement, array $params = [], array $types = [], $isSlave = false)
+    {
+        $cacheId = $this->buildCacheId($statement, $params, 'fetchAssoc');
+        if ($result = $this->getCacheDecodedItem($cacheId, null, $this->cached)) {
+            return $result;
+        }
+
+        if ($result = $this->db->fetchAll($statement, $params, $types, $isSlave)) {
+            $this->setCacheEncodedItem($cacheId, $result, $this->ttl);
+        }
+
+        return $result;
+    }
+
+
+    /**
+     * Prepares and executes an SQL query and returns the result as an associative array.
+     *
+     * @param string $sql    The SQL query.
+     * @param array  $params The query parameters.
+     * @param array  $types  The query parameter types.
+     * @param bool   $isSlave
+     * @return array|false
+     */
+    public function fetchAll($sql, array $params = [], $types = [], $isSlave = false)
+    {
+        $cacheId = $this->buildCacheId($sql, $params, 'fetchAll');
+        if ($result = $this->getCacheDecodedItem($cacheId, null, $this->cached)) {
+            return $result;
+        }
+
+        if ($result = $this->db->fetchAll($sql, $params, $types, $isSlave)) {
+            $this->setCacheEncodedItem($cacheId, $result, $this->ttl);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Executes an, optionally parametrized, SQL query.
+     * If the query is parametrized, a prepared statement is used.
+     * If an SQLLogger is configured, the execution is logged.
+     *
+     * @param string $query  The SQL query to execute.
+     * @param array  $params The parameters to bind to the query, if any.
+     * @param array  $types  The types the previous parameters are in.
+     * @param bool   $isSlave
+     * @return array|Statement|false The executed statement.
+     */
+    public function fetchPairs($query, array $params = [], $types = [], $isSlave = false)
+    {
+        $cacheId = $this->buildCacheId($query, $params, 'fetchPairs');
+        if ($result = $this->getCacheDecodedItem($cacheId, null, $this->cached)) {
+            return $result;
+        }
+
+        if ($result = $this->db->fetchPairs($query, $params, $types, $isSlave)) {
+            $this->setCacheEncodedItem($cacheId, $result, $this->ttl);
         }
 
         return $result;
