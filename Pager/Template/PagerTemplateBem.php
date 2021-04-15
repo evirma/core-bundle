b635060f6d5ebb5da7b76a90133445ac28672f09
Eugene Myazin <meniam@gmail.com>
2021-04-15 16:19:21 +0300

    Page Bem

--- Pager/Template/PagerTemplateBem.php
+++ Pager/Template/PagerTemplateBem.php
@@ -94,7 +94,7 @@ class PagerTemplateBem extends AbstractPagerTemplate
         $nextPageText = ($this->locale == 'ru') ? 'Следующая страница' : 'Show More';
         $navNextLabel = ($this->locale == 'ru') ? 'Быстрая навигация' : 'Fast Navigation';
 
-        return "<nav class=\"pager__next\"><ul class=\"pager__next-list\" aria-label=\"$navNextLabel\">$nextPageButtonPrepend<li class=\"pager__next-item\"><a{$rel}class=\"pager__next-link\" href=\"$href\">$nextPageText &rarr;</a></li></ul></nav>";
+        return "<nav class=\"pager__next\"><ul class=\"pager__next-list\" aria-label=\"$navNextLabel\">$nextPageButtonPrepend<li class=\"pager__next-item\"><a$rel class=\"pager__next-link\" href=\"$href\">$nextPageText &rarr;</a></li></ul></nav>";
     }
 
     public function separator()
