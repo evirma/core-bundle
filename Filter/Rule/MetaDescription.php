b7b746251390cdab5a06a40ece3222e799ba7fb1
Eugene Myazin <meniam@gmail.com>
2022-01-30 12:06:23 +0300

    MetaDesc

--- Filter/Rule/MetaDescription.php
+++ Filter/Rule/MetaDescription.php
@@ -4,13 +4,16 @@ namespace Evirma\Bundle\CoreBundle\Filter\Rule;
 
 use Evirma\Bundle\CoreBundle\Filter\FilterRule;
 use Evirma\Bundle\CoreBundle\Filter\FilterStatic;
-use Evirma\Bundle\CoreBundle\Util\StringUtil;
 
 class MetaDescription extends FilterRule
 {
     public function filter($value)
     {
-        preg_replace('#^\s*<h\d+[^>]>(.*?)</h\d+>#usi', '', $value);
+        if (is_null($value)) {
+            return '';
+        }
+
+        $value = preg_replace('#^\s*<h\d+[^>]>(.*?)</h\d+>#usi', '\\1', $value);
         $value = strip_tags($value);
         $value = FilterStatic::filterValue($value, HtmlAndUnicode::class);
         $value = trim(preg_replace('#\s+#', ' ', $value));
